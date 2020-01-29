c64e79060e98d7f21da49072c1d68e65ee505f3e
chrisdangnguyen <cdnguyen24@gmail.com>
2020-01-29 09:32:51 -0800

    icon and bubbles

---
+++ src/data.js
@@ -0,0 +1,209 @@
+
+let data = [
+  {
+    cat: "San Francisco",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/san_francisco.svg",
+    desc: "what it dodo babeeee"
+  },
+  {
+    cat: "San Francisco",
+    name: "SE 2",
+    value: 2,
+    icon: "../img/san_francisco.svg",
+    desc: ""
+  },
+  {
+    cat: "San Francisco",
+    name: "SE 3",
+    value: 3,
+    icon: "../img/san_francisco.svg",
+    desc: "fniafnajnsjnaoinosnfsngosingsns fn;anfjanfjanfjanjanakjnjasfnjkafnjanajnnajfnafnanjkfnanfnakjfnak;nfjknjkanfjkanakn;fjkagnjf nfosng sngos;n osngangsjngsjngs ijanf bfiahbfaifbnaijnfjka njafnbajkfnajkfnbajfnajkfnajkfnskjj sfjkfns jbnjkfkafnskj.fnbskjfnbs.kjs"
+  },
+  {
+    cat: "San Francisco",
+    name: "SE 4",
+    value: 4,
+    icon: "../img/san_francisco.svg",
+    desc: ""
+  },
+  {
+    cat: "Los Angeles",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/los_angeles.svg",
+    desc: ""
+  },
+  {
+    cat: "Los Angeles",
+    name: "SE 2",
+    value: 2,
+    icon: "../img/los_angeles.svg",
+    desc: ""
+  },
+  {
+    cat: "Los Angeles",
+    name: "SE 3",
+    value: 3,
+    icon: "../img/los_angeles.svg",
+    desc: ""
+  },
+  {
+    cat: "New York",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/new_york.svg",
+    desc: ""
+  },
+  {
+    cat: "New York",
+    name: "SE 2",
+    value: 2,
+    icon: "../img/new_york.svg",
+    desc: ""
+  },
+  {
+    cat: "Seattle",
+    name: "Se 3",
+    value: 1,
+    icon: "../img/seattle.svg",
+    desc: ""
+  },
+  {
+    cat: "Seattle",
+    name: "SE 1",
+    value: 2,
+    icon: "../img/seattle.svg",
+    desc: ""
+  },
+  {
+    cat: "Seattle",
+    name: "SE 1",
+    value: 3,
+    icon: "../img/seattle.svg",
+    desc: ""
+  },
+  {
+    cat: "Dallas",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/dallas.svg",
+    desc: ""
+  },
+  {
+    cat: "Dallas",
+    name: "SE 1",
+    value: 2,
+    icon: "../img/dallas.svg",
+    desc: ""
+  },
+  {
+    cat: "Miami",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/miami.svg",
+    desc: ""
+  },
+  {
+    cat: "Miami",
+    name: "SE 1",
+    value: 3,
+    icon: "../img/miami.svg",
+    desc: ""
+  },
+  {
+    cat: "Chicago",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/chicago.svg",
+    desc: ""
+  },
+  {
+    cat: "Chicago",
+    name: "SE 1",
+    value: 2,
+    icon: "../img/chicago.svg",
+    desc: ""
+  },
+  {
+    cat: "Chicago",
+    name: "SE 1",
+    value: 3,
+    icon: "../img/chicago.svg",
+    desc: ""
+  },
+  {
+    cat: "Philadelphia",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/philadelphia.svg",
+    desc: ""
+  },
+  {
+    cat: "Philadelphia",
+    name: "SE 1",
+    value: 2,
+    icon: "../img/philadelphia.svg",
+    desc: ""
+  },
+  {
+    cat: "Philadelphia",
+    name: "SE 1",
+    value: 4,
+    icon: "../img/philadelphia.svg",
+    desc: ""
+  },
+  {
+    cat: "Philadelphia",
+    name: "SE 1",
+    value: 3,
+    icon: "../img/philadelphia.svg",
+    desc: ""
+  },
+  {
+    cat: "San Diego",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/san_diego.svg",
+    desc: ""
+  },
+  {
+    cat: "San Diego",
+    name: "SE 1",
+    value: 2,
+    icon: "../img/san_diego.svg",
+    desc: ""
+  },
+  {
+    cat: "San Diego",
+    name: "SE 1",
+    value: 3,
+    icon: "../img/san_diego.svg",
+    desc: ""
+  },
+  {
+    cat: "Boston",
+    name: "SE 1",
+    value: 1,
+    icon: "../img/boston.svg",
+    desc: ""
+  },
+  {
+    cat: "Boston",
+    name: "SE 1",
+    value: 2,
+    icon: "../img/boston.svg",
+    desc: ""
+  },
+  {
+    cat: "Boston",
+    name: "SE 1",
+    value: 3,
+    icon: "../img/boston.svg",
+    desc: ""
+  }
+];
+
+
+module.exports = data;
